f65f05b837c885558af2103347113823591d5acf
Henry <henryj654@gmail.com>
2017-04-24 23:31:50 +0300

    testing shiftreg work
    don't know how to time shiftreg serial out properly

--- SRC/gpio.c
+++ SRC/gpio.c
@@ -16,9 +16,15 @@ gpio_init()
 **/
 {
     //!TODO! Figure out pins for shiftreg.
+    P2DIR |= GPIO_PIN2;              // clock for shiftreg
+    P2SEL |= GPIO_PIN2;              /* clock set */
+    GPIO_setAsOutputPin (GPIO_PORT_P6, GPIO_PIN6);     /* Pin 6.6 as serial data output for shiftreg */
+    GPIO_setAsOutputPin (GPIO_PORT_P3, GPIO_PIN2);     /* Pin 3.2 as strobe input for shiftreg*/
+
     // use actual offboard buttons (if so, change btn cnfg)?
     GPIO_setAsInputPinWithPullUpResistor (GPIO_PORT_P1, GPIO_PIN1);     /* Pin 1.1 as Input for on board button */
-    GPIO_setAsInputPinWithPullUpResistor (GPIO_PORT_P2, GPIO_PIN1);     /* Pin 2.1 as Input for on board button */
+    //GPIO_setAsInputPinWithPullUpResistor (GPIO_PORT_P2, GPIO_PIN1);     /* Pin 2.1 as Input for on board button */
+
 
 }
 
--- SRC/main.c
+++ SRC/main.c
@@ -1,10 +1,67 @@
 #include <msp430.h> 
+#include <driverlib.h>
+#include "clock.h"
+#include "gpio.h"
+#include <stdio.h>
 
 /*
  * main.c
  */
+
+void doStuff();
+
+unsigned int i = 0;
+
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+    gpio_init();
+    printf("init test \n");
+    while(1){
+        doStuff();
+    }
 	return 0;
+}
+
+
+void
+outPut()
+{
+    //for (i=0;i<8;i++){
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PIN6);
+        //__delay_cycles(1U);
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PIN6);
+       // __delay_cycles(1U);
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PIN6);
+       // __delay_cycles(1U);
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PIN6);
+       // __delay_cycles(1U);
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PIN6);
+       // __delay_cycles(1U);
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PIN6);
+       // __delay_cycles(1U);
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PIN6);
+       // __delay_cycles(1U);
+        GPIO_toggleOutputOnPin(gpio_PORT_P6, gpio_PORT_P6);
+   // }
+}
+
+
+void
+doStuff()
+{
+    if(! gpio_getPinInput(gpio_PORT_P1, gpio_PIN1))
+    {
+        printf("btn press \n");
+        gpio_setPinHigh(gpio_PORT_P3, gpio_PIN2);
+        outPut();
+        gpio_setPinLow(gpio_PORT_P3, gpio_PIN2);
+    }
+    else{
+    gpio_setPinLow(gpio_PORT_P3, gpio_PIN2);
+    printf("no btn press \n");
+    }
+    __delay_cycles(10000U);
+
 }
